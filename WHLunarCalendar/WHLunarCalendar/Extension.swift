027863ce99549ccc3af3306f19ecfd95bbaaf142
kimwoohyun <dngus242@naver.com>
2017-08-25 17:26:12 +0900

    Init day

--- WHLunarCalendar/WHLunarCalendar/Extension.swift
+++ WHLunarCalendar/WHLunarCalendar/Extension.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 extension UIColor {
-    class var onemomentNavy: UIColor {
+    class var navy: UIColor {
         return UIColor(red: 34.0 / 255.0, green: 41.0 / 255.0, blue: 68.0 / 255.0, alpha: 1.0)
     }
     class var darkGray: UIColor {
-        return UIColor(white: 66.0 / 255.0, alpha: 1.0)
+        return UIColor(red: 121.0 / 255.0, green: 121.0 / 255.0, blue: 121.0 / 255.0, alpha: 1.0)
+    }
+    class var gray: UIColor {
+        return UIColor(red: 204.0 / 255.0, green: 204.0 / 255.0, blue: 204.0 / 255.0, alpha: 1.0)
     }
     class var brightGray: UIColor {
-        return UIColor(white: 229.0 / 255.0, alpha: 1.0)
+        return UIColor(red: 251.0 / 255.0, green: 251.0 / 255.0, blue: 251.0 / 255.0, alpha: 1.0)
     }
-    class var onemomentGold: UIColor {
+    class var gold: UIColor {
         return UIColor(red: 137.0 / 255.0, green: 103.0 / 255.0, blue: 78.0 / 255.0, alpha: 1.0)
+    }
+}
+
+extension Date {
+    func add(day: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: day, to: self)!
+    }
+    func add(month: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .month, value: month, to: self)!
+    }
+    func startOfMonth() -> Date {
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self)))!
     }
 }
--- WHLunarCalendar/WHLunarCalendar/LunarCalendarControllerView.swift
+++ WHLunarCalendar/WHLunarCalendar/LunarCalendarControllerView.swift
@@ -12,17 +12,30 @@ import UIKit
 open class LunarCalendarControllerView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     var collectionview: UICollectionView!
     var cellId = "Cell"
+    var fitstDay = Date().startOfMonth()
+    let thisYear = Calendar.current.component(.year, from: Date().startOfMonth())
+    let thisMonth = Calendar.current.component(.month, from: Date().startOfMonth())
+    let thisWeekday = Calendar.current.component(.weekday, from: Date().startOfMonth())
     
     override open func viewWillAppear(_ animated: Bool) {
         super.viewDidLoad()
+        fitstDay = fitstDay.add(day: -(thisWeekday - 1))
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 20))
+        label.text = String(self.thisYear) + "년 " + String(self.thisMonth) + "월"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = UIColor.darkGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label)
         
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: view.frame.width/7, height: view.frame.height/6)
+        layout.itemSize = CGSize(width: view.frame.width/7, height: (view.frame.height - 20)/6)
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 0
         
-        collectionview = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionview = UICollectionView(frame: CGRect(x: 0, y: 20, width: view.frame.width, height: view.frame.height - 20), collectionViewLayout: layout)
         collectionview.dataSource = self
         collectionview.delegate = self
         collectionview.register(WHLunarCalendarCell.self, forCellWithReuseIdentifier: cellId)
@@ -38,24 +51,51 @@ open class LunarCalendarControllerView: UIViewController, UICollectionViewDataSo
     
     public func collectionView(_ cellForItemAtcollectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionview.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath as IndexPath) as! WHLunarCalendarCell
+        
+        let date = fitstDay.add(day: indexPath.row)
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: date)
+        let month = calendar.component(.month, from: date)
+        let day = calendar.component(.day, from: date)
+        
+        if (thisMonth != month) {
+            cell.isUserInteractionEnabled = false
+            cell.alpha = 0.5
+        }
+        else {
+            cell.isUserInteractionEnabled = true
+            cell.alpha = 1.0
+        }
+        cell.DayLabel.text = String(day)
         return cell
     }
 }
 
 class WHLunarCalendarCell: UICollectionViewCell {
+    var DayLabel: UILabel!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.layer.borderColor = UIColor.brightGray.cgColor
+        self.layer.borderColor = UIColor.white.cgColor
         self.layer.borderWidth = 1.0
+        self.layer.backgroundColor = UIColor.brightGray.cgColor
         addViews()
     }
     
     func addViews(){
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
-        label.textAlignment = .center
-        label.text = "A"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(label)
+        DayLabel = UILabel(frame: CGRect(x: 0, y: 0, width: frame.width, height: 20))
+        DayLabel.textAlignment = .right
+        DayLabel.font = UIFont.systemFont(ofSize: 14)
+        DayLabel.textColor = UIColor.darkGray
+        DayLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(DayLabel)
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            backgroundColor = isSelected ? UIColor.navy : UIColor.brightGray
+            self.DayLabel.textColor = isSelected ? UIColor.white : UIColor.darkGray
+        }
     }
 
     required init?(coder aDecoder: NSCoder) {
